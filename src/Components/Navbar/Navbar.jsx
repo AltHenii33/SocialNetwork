46ae73fd6e8cd03db4128a05b3e2a58fc8382e64
AltHenii33 <alterego.lisa@gmail.com>
2021-11-15 19:34:13 +0200

    Navbar chenging

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,105 @@
-import React from 'react';
+import * as React from 'react';
 import s from './Navbar.module.css';
 import { NavLink } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+
+// export default function SelectedListItem() {
+//    const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+//    const handleListItemClick = (event, index) => {
+//      setSelectedIndex(index);
+//    };
+
 
 const Navbar = () => {
-    return <nav className={s.nav}>
-    <div className={s.item}>
-       <NavLink to="/Profile" activeClassName={s.active}>Profile</NavLink>
-    </div>
-    <div className={s.item}> 
-       <NavLink to="/Dialogs" activeClassName={s.active}>Messages</NavLink>
-    </div>
-    <div className={s.item}>
-       <NavLink to="/News" activeClassName={s.active}>News</NavLink>
-    </div>
-    <div className={s.item}>
-       <NavLink to="/Music" activeClassName={s.active}>Music</NavLink>
-    </div>
-    <div className={s.item}>
-       <NavLink to="/Users" activeClassName={s.active}>Users</NavLink>
-    </div>
-    <div className={s.item}>
-       <NavLink to="/Settings" activeClassName={s.active}>Settings</NavLink>
-    </div>
-  </nav>
+
+   const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+   const handleListItemClick = (event, index) => {
+      setSelectedIndex(index);
+   };
+
+   return (
+      <nav className={s.nav}>
+      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: '#344955' }}>
+         <List component="nav" aria-label="secondary mailbox folder">
+            <NavLink to="/Profile" style={{ color: 'white', textDecoration: 'inherit'}}>
+               <ListItemButton 
+                  selected={selectedIndex === 1}
+                  onClick={(event) => handleListItemClick(event, 1)}
+               >
+                  <ListItemText primary="Profile" />
+               </ListItemButton>
+            </NavLink>
+            <NavLink to="/Dialogs" style={{ color: 'white', textDecoration: 'inherit'}}>
+            <ListItemButton
+               selected={selectedIndex === 2}
+               onClick={(event) => handleListItemClick(event, 2)}
+            >
+               <ListItemText primary="Dialogs" />
+            </ListItemButton>
+            </NavLink>
+            <NavLink to="/News" style={{ color: 'white', textDecoration: 'inherit'}}>
+            <ListItemButton
+               selected={selectedIndex === 3}
+               onClick={(event) => handleListItemClick(event, 3)}
+            >
+               <ListItemText primary="News" />
+            </ListItemButton>
+            </NavLink>
+            <NavLink to="/Music" style={{ color: 'white', textDecoration: 'inherit'}}>
+            <ListItemButton
+               selected={selectedIndex === 4}
+               onClick={(event) => handleListItemClick(event, 4)}
+            >
+               <ListItemText primary="Music" />
+            </ListItemButton>
+            </NavLink>
+            <NavLink to="/Users" style={{ color: 'white', textDecoration: 'inherit'}}>
+            <ListItemButton
+               selected={selectedIndex === 5}
+               onClick={(event) => handleListItemClick(event, 5)}
+            >
+               <ListItemText primary="Users" />
+            </ListItemButton>
+            </NavLink>
+            <NavLink to="/Settings" style={{ color: 'white', textDecoration: 'inherit'}}>
+            <ListItemButton
+               selected={selectedIndex === 6}
+               onClick={(event) => handleListItemClick(event, 6)}
+            >
+               <ListItemText primary="Settings" />
+            </ListItemButton>
+            </NavLink>
+         </List>
+      </Box>
+      </nav>
+   );
+
+      return <nav className={s.nav}>
+         <div className={s.item}>
+            <NavLink to="/Profile" activeClassName={s.active}>Profile</NavLink>
+         </div>
+         <div className={s.item}>
+            <NavLink to="/Dialogs" activeClassName={s.active}>Messages</NavLink>
+         </div>
+         <div className={s.item}>
+            <NavLink to="/News" activeClassName={s.active}>News</NavLink>
+         </div>
+         <div className={s.item}>
+            <NavLink to="/Music" activeClassName={s.active}>Music</NavLink>
+         </div>
+         <div className={s.item}>
+            <NavLink to="/Users" activeClassName={s.active}>Users</NavLink>
+         </div>
+         <div className={s.item}>
+            <NavLink to="/Settings" activeClassName={s.active}>Settings</NavLink>
+         </div>
+      </nav>
 }
 
 export default Navbar;
--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import s from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader';
 import ProfileStatus from './ProfileStatus';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import SimpleImageSlider from "react-simple-image-slider";
 
 
 
@@ -13,9 +14,27 @@ const ProfileInfo = (props) => {
     return <Preloader/>
   }
 
+  const images = [
+    { url: "https://n1s1.elle.ru/b4/22/05/b4220547cc9284354a0615d442406c49/728x546_1_8ebf9fc7d9abd6dc934095ecfbe237a0@1016x762_0xc35dbb80_10169127321522236626.jpg" },
+    { url: "https://www.peoples.ru/images/interesting/interesting_201502041205180.jpg" },
+    { url: "https://www.spletnik.ru/img/__post/4f/4fa72c931a76b5829cb974cef2cc7692_698.jpg" },
+    { url: "https://lastmag.ru/wp-content/uploads/2020/04/emma.jpg" },
+    { url: "https://www.publy.ru/wp-content/uploads/2013/09/712.jpg" },
+    { url: "https://mport.ua/i/75/87/23/758723/2835e59f70f088ff5edc31d5303e0c93-quality_70Xresize_crop_1Xallow_enlarge_0Xw_1200Xh_643.jpg" },
+  ];
+
   return <div className={s.Profile}>
     <div className ={s.picture}>
-      <img src='https://klike.net/uploads/posts/2019-06/1561182204_1.jpg' />
+      {/* <img src='https://klike.net/uploads/posts/2019-06/1561182204_1.jpg' /> */}
+      <SimpleImageSlider
+        width={1100}
+        height={304}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+        autoPlay={true}
+        autoPlayDelay={2}
+      />
     </div>
     <div>
       <img src={props.profile.photos.large} /> 
