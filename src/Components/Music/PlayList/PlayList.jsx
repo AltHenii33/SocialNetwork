34f8fc345fb4100768482d804a4ec8009e488b26
AltHenii33 <alterego.lisa@gmail.com>
2021-11-15 14:36:54 +0200

    PlayList
    Add PlayList page

--- src/Components/Music/PlayList/PlayList.jsx
+++ src/Components/Music/PlayList/PlayList.jsx
@@ -5,10 +5,7 @@ const PlayList = () => {
 
   return <div>
     <img src='https://klike.net/uploads/posts/2019-06/1561182204_1.jpg' /> 
-    <img src='https://klike.net/uploads/posts/2019-06/1561182204_1.jpg' /> 
-    <img src='https://klike.net/uploads/posts/2019-06/1561182204_1.jpg' /> 
-    <img src='https://klike.net/uploads/posts/2019-06/1561182204_1.jpg' /> 
-    
+    sadasdasdsdas
 
   </div>
 }
